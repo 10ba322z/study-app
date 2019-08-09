75b848ac6e56d9ab7bfd8f9a8ebc5198c4e2573d
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-08-09 23:03:54 +0900

    change root routes if logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#index'
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'pages#index'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new'
+    end
+  end
   get  'pages/show'
 end
