938856a7cd6651c23a3cefa9ca10925d48081dec
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-09-15 18:23:16 +0900

    create test user seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,4 @@ User.create!(username: "Example User",
              email:    "test@example.com",
              password:              "testtest",
              password_confirmation: "testtest",
-             activated: true,
-             activated_at: Time.zone.now)
+             )
