c8606eb48b14584e07165b199bbb0daa7be6d6ee
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-09-16 22:01:14 +0900

    add db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,19 @@
-User.create!(username: "Example User",
-             email:    "test@example.com",
-             password:              "testtest",
-             password_confirmation: "testtest",
-             )
+99.times do |n|
+  name = "sample_user_#{n+1}"
+  email    = "sample_user_#{n+1}@sample.com"
+  password = "password_#{n+1}"
+  user = User.new(username:  name,
+                  email: email,
+                  password:              password,
+                  password_confirmation: password)
+  user.skip_confirmation!
+  user.save
+end
+
+users = User.order(:created_at).take(7)
+30.times do |c|
+  content  = "投稿テスト#{c+1}"
+  start_at = Time.current - c.day - rand(1..8).hours
+  end_at   = Time.current - c.day
+  users.each { |user| user.microposts.create!(content: content, start_at: start_at, end_at: end_at) }
+end
