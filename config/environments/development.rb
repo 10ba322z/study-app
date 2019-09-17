8e43c7ac65b1e8b2097b2c50ff01974c89429bf7
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-09-17 16:07:01 +0900

    fix production.rb mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,11 +31,19 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener_web
+  # config.action_mailer.delivery_method = :letter_opener_web
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["MAILER_SENDER"],
+    password: ENV["MAILER_PASS"],
+    enable_starttls_auto: true,
+    authentication: "plain",
+    }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
