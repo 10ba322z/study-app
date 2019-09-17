38e7595fc3a1bf56819d1959be0d5b8f8298cb8c
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-09-17 14:50:30 +0900

    mail send test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "study-app_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "smtp.gmail.com",
+    user_name: ENV["MAILER_SENDER"],
+    password: ENV["MAILER_PASS"],
+    enable_starttls_auto: true,
+    authentication: "plain",
+    }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
