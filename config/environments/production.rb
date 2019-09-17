04fbb0dd122547687f6712fc89a80279e2b64489
10ba322z <10ba322z@10ba322zettonoMacBook-Air.local>
2019-09-17 16:13:19 +0900

    fix production.rb. host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
+  host = 'study-app.net'
+  config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
